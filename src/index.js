016b2f87618facf9b533ab135ba9d9fb3cfe76de
OlegPetin <olegpetin87@gmail.com>
2023-01-05 16:16:10 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
+    if (n < 0){
+        n=n*(-1);
+    }
+    St = n.toString();
+    let newSt = '';
+    for (let i = St.length - 1; i>=0; i--) {
+        newSt += St[i];
+    }
+    return newSt;
+
+    /*if (st[0] == '-') {
+        st = st.substring(1,(st.length-1));
+    } else {
+        
+    }*/
+
   
 }
